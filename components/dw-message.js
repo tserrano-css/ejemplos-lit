72e404d4dd5658579846ac3f6666c52b36f0a069
Toni Serrano Suarez <tserrano@cambrasabadell.org>
2022-05-13 20:04:31 +0200

    first init

---
+++ components/dw-message.js
@@ -0,0 +1,53 @@
+import { LitElement, html, css } from "lit";
+
+class DwMessage extends LitElement {
+  static styles = css`
+    :host {
+      display: block;
+      border: 1px solid red;
+      padding: 10px;
+    }
+    div {
+        display: none;
+      background-color: #fcc;
+    }
+    p {
+      margin-bottom: 0;
+    }
+    :host([show]) div {
+        display: block;
+    }
+  `;
+
+  static properties = {
+    msg: {
+      type: String,
+      attribute: "message",
+      state: false,
+    },
+    show: {
+      type: Boolean,
+      attribute: "show",
+      reflect: true,
+    },
+  };
+
+  constructor() {
+    super();
+    this.msg = "Bienvenido al mundo de LitElement";
+    this.show = false;
+  }
+
+  render() {
+    return html`
+      <div>${this.msg}</div>
+      <button @click=${this.toggle}>${this.show ? "Ocultar" : "Mostrar"}</button>
+    `;
+  }
+
+  toggle() {
+    this.show = !this.show;
+  }
+}
+
+customElements.define("dw-message", DwMessage);
